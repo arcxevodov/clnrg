e5b176ec4f1e5a9fbd7f21a3a3b332ad1b92a9dd
Артур Арчеводов <artur.archevodov@gmail.com>
2023-01-14 10:30:32 +0400

    Добавил локализации

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,6 +13,9 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 )
 
 const (
@@ -18,7 +23,10 @@ const (
 	UnknownError = 2
 )
 
+var localeArg = flag.String("lang", "en", "Set locale")
+
 func main() {
+	flag.Parse()
 	a := app.New()
 
 	_, err := exec.Command("clnr", "-i").Output()
@@ -26,10 +34,10 @@ func main() {
 		if err.Error() == "exec: \"clnr\": executable file not found in $PATH" {
 			noClnr(a).Show()
 		} else if err.Error() == "exit status 1" {
-			fmt.Println("\033[0;31mPlease run this program as superuser")
+			fmt.Println("\033[0;31m" + localString("NoRoot"))
 			os.Exit(NoRootError)
 		} else {
-			fmt.Println("Unknown Error: ", err)
+			fmt.Println(localString("UnknownError"), err)
 			os.Exit(UnknownError)
 		}
 	} else {
@@ -39,10 +47,61 @@ func main() {
 	a.Run()
 }
 
-func updateRam(ram *widget.Label) {
-	out, err := exec.Command("clnr", "-i").Output()
+func initLocalizer() *i18n.Localizer {
+	var bundle *i18n.Bundle
+	var localizer *i18n.Localizer
+
+	path, err := os.Executable()
 	if err != nil {
-		fmt.Println("Unknown Error: ", err)
+		fmt.Println(localString("UnkownError"), err)
+		os.Exit(UnknownError)
+	}
+
+	if *localeArg == "ru" {
+		bundle = i18n.NewBundle(language.Russian)
+		bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+		_, err := bundle.LoadMessageFile(path[:len(path)-5] + "/locales/ru.json")
+		if err != nil {
+			fmt.Println(localString("UnknownError"), err)
+			os.Exit(UnknownError)
+		}
+		localizer = i18n.NewLocalizer(bundle, language.Russian.String())
+	} else {
+		bundle = i18n.NewBundle(language.English)
+		bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
+		_, err := bundle.LoadMessageFile(path[:len(path)-5] + "/locales/en.json")
+		if err != nil {
+			fmt.Println(localString("UnknownError"), err)
+			os.Exit(UnknownError)
+		}
+		localizer = i18n.NewLocalizer(bundle, language.English.String())
+	}
+	return localizer
+}
+
+func localString(id string) string {
+	localizer := initLocalizer()
+	localzeConfig := i18n.LocalizeConfig{
+		MessageID: id,
+	}
+	result, err := localizer.Localize(&localzeConfig)
+	if err != nil {
+		fmt.Println(localString("UnknownError"), err)
+		os.Exit(UnknownError)
+	}
+	return result
+}
+
+func updateRam(ram *widget.Label) {
+	var out []byte
+	var err error
+	if *localeArg == "ru" {
+		out, err = exec.Command("clnr", "-i", "-lang=ru").Output()
+	} else {
+		out, err = exec.Command("clnr", "-i").Output()
+	}
+	if err != nil {
+		fmt.Println(localString("UnknownError"), err)
 		os.Exit(UnknownError)
 	}
 	ram.SetText(string(out))
@@ -59,10 +118,16 @@ func mainWidget(a fyne.App) fyne.Window {
 		}
 	}()
 
-	clean := widget.NewButton("Clean RAM", func() {
-		info, err := exec.Command("clnr", "-r", "-s").Output()
+	clean := widget.NewButton(localString("CleanRamButton"), func() {
+		var info []byte
+		var err error
+		if *localeArg == "ru" {
+			info, err = exec.Command("clnr", "-r", "-s", "-lang=ru").Output()
+		} else {
+			info, err = exec.Command("clnr", "-r", "-s").Output()
+		}
 		if err != nil {
-			fmt.Println("Unknown Error: ", err)
+			fmt.Println(localString("UnknownError"), err)
 			os.Exit(UnknownError)
 		}
 		cleanSucsessful(a, string(info)).Show()
@@ -74,14 +139,14 @@ func mainWidget(a fyne.App) fyne.Window {
 }
 
 func noClnr(a fyne.App) fyne.Window {
-	w := a.NewWindow("clnr not found")
-	prompt := widget.NewLabel("\"clnr\" executable file not found. Please run install_clnr.sh")
+	w := a.NewWindow(localString("NoClnrTitle"))
+	prompt := widget.NewLabel(localString("ClnrNotFound"))
 	w.SetContent(prompt)
 	return w
 }
 
 func cleanSucsessful(a fyne.App, out string) fyne.Window {
-	w := a.NewWindow("Cleaning...")
+	w := a.NewWindow(localString("CleanSuccessfulTitle"))
 	logging := widget.NewLabel(out)
 	w.SetContent(logging)
 	return w
